586a6d12ac6b40d8fded367bcd6fa22a82374fe6
manikantadeveloper-143 <manikanta7013@gmail.com>
2024-05-16 18:43:44 +0530

    Whatsapp Task

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+    theme: {
+      extend: {
+        screens: {
+          "whatsapp-bp": "755px",
+        },
+      },
+    },
+    plugins: [],
+  };
+  
